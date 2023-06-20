d8a287f84ee4aa73f271305fb5cf0ab7fb8c5471
dlehdduq22 <dlehdduq22@naver.com>
2023-06-20 19:40:32 +0900

    댓글과 게시글 번호 찾기 servlet

---
+++ main/java/servlet/SelectPostByIdServlet.java
@@ -0,0 +1,36 @@
+package servlet;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.CommentDao;
+import dao.PostDao;
+import vo.CommentVO;
+import vo.PostVO;
+
+@WebServlet("/SelectPostByIdServlet")
+public class SelectPostByIdServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int post_Mno = Integer.parseInt(request.getParameter("post_mno"));
+		
+		PostDao postDao = new PostDao();
+		PostVO postVO = postDao.selectPostById(post_Mno);
+		
+		request.setAttribute("postVO", postVO);
+		
+		CommentDao commentDao = new CommentDao();
+		List<CommentVO> comments = commentDao.selectCommentsByPostId(post_Mno);
+		request.setAttribute("comments", comments);
+		
+		request.getRequestDispatcher("postDetail.jsp").forward(request, response);	
+	}
+
+}
